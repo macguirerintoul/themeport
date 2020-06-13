e4afd37262c7cf5cbbaee8075dcff87abcb19578
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-06-13 11:43:54 -0700

    only include vim values that exist

--- utilities.js
+++ utilities.js
@@ -1,5 +1,12 @@
 module.exports = {
+	/**
+	 * Sanitize a hex string to remove the alpha channel.
+	 *
+	 * @param {string} hex - the string to sanitize
+	 * @return {string} the sanitized string
+	 */
 	removeAlphaFromHex: function(hex) {
+		if (!hex) return;
 		if (hex.length === 7) return hex;
 		if (hex.length === 6) return "#" + hex;
 		if (hex.length === 9) return hex.substr(0, 7);
